3ac412a2e24febd9a32b770d223741e18ff9f552
likhitha <plikhithareddy79@gmail.com>
2016-07-07 22:04:04 +0530

    did some changes to use card class as library  for diamond game

--- Card.java
+++ Card.java
@@ -3,22 +3,23 @@ public class Card  {
 	char pip;
 	String suit;
 	
-	public Card(char pip, char suit) {
+	public Card(char pip, String suit) {
 		this.pip = pip;
 		this.suit = suit;
 	}
 
 	public int compare(Card c) {
-		return  getPip() - c.getPip()	
+		return  getPip() - c.getPip();
 	}
 
 	public int getPip() {
 		String pipString = " A23456789TJQK";
 		return pipString.indexOf(pip);
 	}
-        public String getPip() {
+        public String getSuit() {
 		
 		return suit;
-	}          
+	} 
+         
 	
 }
