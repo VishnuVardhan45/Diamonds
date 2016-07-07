952bd7a4e56a84dd7290ddeb7e6a68605665ef2e
likhitha <plikhithareddy79@gmail.com>
2016-07-07 21:39:57 +0530

    modified compare method

--- Card.java
+++ Card.java
@@ -8,18 +8,8 @@ public class Card  {
 		this.suit = suit;
 	}
 
-	public String compare(Card c) {
-		
-		//String suitString = "CDHS";
-		int card1Value = getPip(); //+ 13 * suitString.indexOf(suit);
-		
-		int card2Value = getPip(); //+ 13 * suitString.indexOf(c.suit);	
-		int difference = card1Value - card2Value;
-
-		if (difference < 0)
-			return "Card 2 is greater";
- 		else
-			return "Card 1 is greater";	
+	public int compare(Card c) {
+		return  getPip() - c.getPip()	
 	}
 
 	public int getPip() {
