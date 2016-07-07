ae0e58cf89fe16c7e488f47e301d4be5888683b8
likhitha <plikhithareddy79@gmail.com>
2016-07-07 15:10:47 +0530

    added TestCard

---
+++ Testcard.java
@@ -0,0 +1,12 @@
+public class Testcard {
+
+	public static void main(String[] args) {
+  		Card c1 = new Card('1','S');
+   		Card c2 = new Card('4','S');
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c1.compare(c2));
+	}
+}
+  
+ 
