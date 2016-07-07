01e86c052af99a7ef2001bdf288cade7333d9c21
likhitha <plikhithareddy79@gmail.com>
2016-07-07 22:12:45 +0530

    Modified constructor of Player.java

--- Player.java
+++ Player.java
@@ -7,5 +7,20 @@ public class Player {
 	Player(char suit) {
 		this.suit = suit; 
 		score = 0;
+		available = new ArrayList<>();
+		available.add(new Card('A', suit));
+		available.add(new Card('2', suit));
+		available.add(new Card('3', suit));
+		available.add(new Card('4', suit));
+		available.add(new Card('5', suit));
+		available.add(new Card('6', suit));
+		available.add(new Card('7', suit));
+		available.add(new Card('8', suit));
+		available.add(new Card('9', suit));
+		available.add(new Card('J', suit));
+		available.add(new Card('Q', suit));
+		available.add(new Card('K', suit));
 	}
+
+	
 }
