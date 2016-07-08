5f34b07b4c6db0827101a426daa0d29f25398262
likhitha <plikhithareddy79@gmail.com>
2016-07-08 09:26:58 +0530

    changed suit from string to char

--- Testcard.java
+++ Testcard.java
@@ -1,10 +1,10 @@
 public class Testcard {
 
 	public static void main(String[] args) {
-  		Card c1 = new Card('1','S');
+  		Card c1 = new Card('A','S');
    		Card c2 = new Card('4','S');
-		System.out.println(c1);
-		System.out.println(c2);
+		System.out.println(c1.getPip() + "of" + c1.getSuit());
+		System.out.println(c2.getPip() + "of" + c2.getSuit());
 		System.out.println(c1.compare(c2));
 	}
 }
