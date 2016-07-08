7e78b6da36082adc981d27109c654965a489f15f
Archana Kumari <Archana.Kumari@cdk.com>
2016-07-08 07:41:30 +0530

    Added playRound() to DiamondGame.java

--- DiamondGame.java
+++ DiamondGame.java
@@ -1,8 +1,13 @@
 import java.util.*;
 public class DiamondGame {
 	ArrayList<Card> diamondCards;
+	Player computer;
+	Player user;
+	
 	DiamondGame() {
 		diamondCards = new ArrayList<Integer>();
+		computer = new Player('S');
+		user = new Player('H');
 		String pipValues = "A23456789TJQK";
 		for(int i = 0; i < pipValues.length(); i++) {
 			diamondCard.add(new Card(pipValues.charAt(i),'D'));
@@ -10,8 +15,24 @@ public class DiamondGame {
 		Collections.shuffle(diamondCards);
 	}
 
-	public static int topOfStock(ArrayList<Integer> tos) {
-		return tos.get(0);
+	public Card topOfStock() {
+		Card c = diamondCards.get(0);
+		diamondCards.remove(0); 		
+		return c;
 	}
-
+	
+	public playRound(Player p1, Player p2) {
+		Card c1 = p1.bid();
+		Card c2 = p2.bid();		
+		int diffn = c1.compare(c2);	
+		int valOfDiam = topOfStock().getPip();		
+		if (diffn < 0) 
+			p2.updateScore(valOfDiam);
+		else if (diffn > 0) 
+			p1.updateScore(valOfDiam);
+		else {
+			p1.updateScore(valOfDiam/2);
+			p2.updateScore(valOfDiam/2);
+		}
+	}
 }
