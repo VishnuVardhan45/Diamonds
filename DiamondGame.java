df38f1f5339416a903409c03a92b7300f757a228
Archana Kumari <Archana.Kumari@cdk.com>
2016-07-08 07:21:18 +0530

    added constructor to DiamonGame.jaav

--- DiamondGame.java
+++ DiamondGame.java
@@ -1,22 +1,17 @@
 import java.util.*;
 public class DiamondGame {
-
-	public static int diamondCards() {
-		String pipValues = " A23456789TJQK";
-		ArrayList<Integer> diamondCard = new ArrayList<Integer>();
-
-		for(int i = 1; i < pipValues.length(); i++) {
-			diamondCard.add((pipValues.indexOf(pipValues.charAt(i))));
-				}
-		return TopofStock(shuffleDiamonds(diamondCard));
-	}
-
-	public static ArrayList<Integer> shuffleDiamonds(ArrayList<Integer> s) {
-		Collections.shuffle(s);
-		return s;
-					
+	ArrayList<Card> diamondCards;
+	DiamondGame() {
+		diamondCards = new ArrayList<Integer>();
+		String pipValues = "A23456789TJQK";
+		for(int i = 0; i < pipValues.length(); i++) {
+			diamondCard.add(new Card(pipValues.charAt(i),'D'));
 		}
-	public static int topofStock(ArrayList<Integer> tos) {
-					return tos.get(1);
-			}
+		Collections.shuffle(diamondCards);
 	}
+
+	public static int topOfStock(ArrayList<Integer> tos) {
+		return tos.get(0);
+	}
+
+}
