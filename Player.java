d6cf3ed07ee5c9f0d5a81dc2913721b3540304b8
Archana Kumari <Archana.Kumari@cdk.com>
2016-07-08 07:00:54 +0530

    Added bit methd and removed compile time errors

--- Player.java
+++ Player.java
@@ -1,25 +1,34 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Random;
 public class Player {
 	char suit;
-	ArrayList<Card>  available;
-	int valueAccumulated;
+	ArrayList<Card>  availableCards;
+	int score;
 
 	Player(char suit) {
 		this.suit = suit; 
 		score = 0;
-		available = new Arraylist<>();
-		available.put(new Card('A',suit));
-		available.put(new Card('2',suit));
-		available.put(new Card('3',suit));
-		available.put(new Card('4',suit));
-		available.put(new Card('5',suit));
-		available.put(new Card('6',suit));
-		available.put(new Card('7',suit));
-		available.put(new Card('8',suit));
-		available.put(new Card('9',suit));
-		available.put(new Card('T',suit));
-		available.put(new Card('J',suit));
-		available.put(new Card('Q',suit));
-		available.put(new Card('K',suit));		
+		availableCards = new ArrayList<>();
+		availableCards.add(new Card('A',suit));
+		availableCards.add(new Card('2',suit));
+		availableCards.add(new Card('3',suit));
+		availableCards.add(new Card('4',suit));
+		availableCards.add(new Card('5',suit));
+		availableCards.add(new Card('6',suit));
+		availableCards.add(new Card('7',suit));
+		availableCards.add(new Card('8',suit));
+		availableCards.add(new Card('9',suit));
+		availableCards.add(new Card('T',suit));
+		availableCards.add(new Card('J',suit));
+		availableCards.add(new Card('Q',suit));
+		availableCards.add(new Card('K',suit));		
+	}
+
+	public Card bid() {
+		Random r = new Random();
+		int index = r.nextInt(availableCards.size());
+		Card bidding = availableCards.get(index);
+		availableCards.remove(index);
+		return bidding;
 	}
 }
